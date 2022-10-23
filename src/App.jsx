c1ad021dfc50736403df26bfef6e0314fdb5c248
Jesus Vergara <jesusmanuelv1989@gmail.com>
2022-10-23 10:34:03 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import PhotosAppScriptClass1 from './components/PhotosAppScriptClass1'
+
+function App() {
+
+  return (
+    <div className="App">
+
+      <PhotosAppScriptClass1/>
+      
+    </div>
+  )
+}
+
+export default App
+  
